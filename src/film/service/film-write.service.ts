3aa5061a82f4c565069aa8ab82f40f5209014d34
Kevin Goll <goke1012@h-ka.de>
2023-05-03 14:22:55 +0200

    Started implementing film-write.service

---
+++ src/film/service/film-write.service.ts
@@ -0,0 +1,37 @@
+/**
+ * Das Modul besteht aus der Klasse {@linkcode FilmWriteService} für die
+ * Schreiboperationen im Anwendungskern.
+ * @packageDocumentation
+ */
+
+import { Film } from '../entity/film.entity';
+import { FilmReadService } from './film-read.service.js';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+import { MailService } from '../../mail/mail.service.js';
+import RE2 from 're2';
+import { Repository } from 'typeorm';
+import { getLogger } from '../../logger/logger';
+
+@Injectable()
+export class FilmWriteService {
+    private static readonly VERSION_PATTERN = new RE2('^"\\d*"');
+
+    readonly #repo: Repository<Film>;
+
+    readonly #readService: FilmReadService;
+
+    readonly #mailService: MailService;
+
+    readonly #logger = getLogger(FilmWriteService.name);
+
+    constructor(
+        @InjectRepository(Film) repo: Repository<Film>,
+        readService: FilmReadService,
+        mailService: MailService,
+    ) {
+        this.#repo = repo;
+        this.#readService = readService;
+        this.#mailService = mailService;
+    }
+}
