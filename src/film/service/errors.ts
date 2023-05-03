ca6e7711fc7a32edead8449f81f430f9845fa0de
Kevin Goll <goke1012@h-ka.de>
2023-05-03 20:35:39 +0200

    Implemented update-function and corresponding validation-function incl. error-type for write-operations.

--- src/film/service/errors.ts
+++ src/film/service/errors.ts
@@ -6,11 +6,37 @@
 import { Suchkriterien } from './film-read.service';
 
 /**
- * Klasse für eine bereits existierende ID.
+ * Klasse für einen bereits existierenden Film.
  */
 export interface MovieExists {
     readonly type: 'MovieExists';
-    readonly suchkriterien: Suchkriterien;
+    readonly suchkriterien?: Suchkriterien;
+    readonly id?: number;
+}
+
+/**
+ * Klasse für einen nicht bereits existierenden Film.
+ */
+export interface MovieDoesNotExist {
+    readonly type: 'MovieDoesNotExist';
+    readonly id: number | undefined;
+}
+
+/**
+ * Klasse für eine ungültige Versionsnummer beim Ändern.
+ */
+export interface VersionInvalid {
+    readonly type: 'VersionInvalid';
+    readonly version: string | undefined;
+}
+
+/**
+ * Klasse für eine veraltete Versionsnummer beim Ändern.
+ */
+export interface VersionOutdated {
+    readonly type: 'VersionOutdated';
+    readonly id: number;
+    readonly version: number;
 }
 
 /**
@@ -18,3 +44,11 @@ export interface MovieExists {
  * - {@linkcode MovieExists}
  */
 export type CreateError = MovieExists;
+
+/**
+ * Union-Type für die möglichen Fehler beim Ändern eines Films in der DB:
+ * - {@linkcode MovieDoesNotExist}
+ * - {@linkcode VersionInvalid}
+ * - {@linkcode VersionOutdated}
+ */
+export type UpdateError = MovieDoesNotExist | VersionInvalid | VersionOutdated;
--- src/film/service/film-write.service.ts
+++ src/film/service/film-write.service.ts
@@ -4,8 +4,14 @@
  * @packageDocumentation
  */
 
+import {
+    CreateError,
+    MovieDoesNotExist,
+    UpdateError,
+    VersionInvalid,
+    VersionOutdated,
+} from './errors';
 import { FilmReadService, Suchkriterien } from './film-read.service.js';
-import { CreateError } from './errors';
 import { Film } from '../entity/film.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
@@ -13,6 +19,15 @@ import RE2 from 're2';
 import { Repository } from 'typeorm';
 import { getLogger } from '../../logger/logger';
 
+/** Interface für die Informationen, die zum Updaten eines Films notwendig sind */
+export interface UpdateParams {
+    /** ID des Films */
+    id: number | undefined;
+    /** Film mit aktuellen Werten */
+    film: Film;
+    /** Versionsnummer für die neuen Daten des Films */
+    version: string;
+}
 @Injectable()
 export class FilmWriteService {
     private static readonly VERSION_PATTERN = new RE2('^"\\d*"');
@@ -50,6 +65,45 @@ export class FilmWriteService {
         return filmDb.id!; // eslint-disable-line @typescript-eslint/no-non-null-assertion
     }
 
+    /**
+     * Einen vorhandenen Film aktualisieren
+     * @param film Der zu aktualisierende Film
+     * @param id ID des Films
+     * @param version Die Versionsnummer für optimistische Synchronisation
+     * @returns Die neue Versionsnummer gemäß optimistischer Synchronisation
+     *  oder im Fehlerfall [UpdateError](../types/film_service_errors.UpdateError.html)
+     */
+    async update({
+        id,
+        film,
+        version,
+    }: UpdateParams): Promise<UpdateError | number> {
+        this.#logger.debug(
+            'update: id=%d, film=%o, version=%s',
+            id,
+            film,
+            version,
+        );
+        if (id === undefined) {
+            this.#logger.debug('update: Ungültige ID angegeben');
+            return { type: 'MovieDoesNotExist', id };
+        }
+
+        const validateResult = await this.#validateUpdate(film, id, version);
+        this.#logger.debug('update: validateResult=%o', validateResult);
+        if (!(validateResult instanceof Film)) {
+            return validateResult;
+        }
+
+        const filmNeu = validateResult;
+        const merged = this.#repo.merge(filmNeu, film);
+        this.#logger.debug('update: merged=%o', merged);
+        const updated = await this.#repo.save(merged);
+        this.#logger.debug('update: updated=%o', updated);
+
+        return updated.version!; // eslint-disable-line @typescript-eslint/no-non-null-assertion
+    }
+
     async #validateCreate(film: Film): Promise<CreateError | undefined> {
         this.#logger.debug('#validateCreate: film=%o', film);
 
@@ -68,5 +122,71 @@ export class FilmWriteService {
 
         this.#logger.debug('#validateCreate: ok');
         return undefined;
+    }
+
+    async #validateUpdate(
+        film: Film,
+        id: number,
+        versionStr: string,
+    ): Promise<Film | UpdateError> {
+        const result = this.#validateVersion(versionStr);
+        if (typeof result !== 'number') {
+            return result;
+        }
+
+        const version = result;
+        this.#logger.debug(
+            '#validateUpdate: film=%o, version=%s',
+            film,
+            version,
+        );
+
+        const resultFindById = await this.#findByIdAndCheckVersion(id, version);
+        this.#logger.debug('#validateUpdate: %o', resultFindById);
+        return resultFindById;
+    }
+
+    #validateVersion(version: string | undefined): VersionInvalid | number {
+        if (
+            version === undefined ||
+            !FilmWriteService.VERSION_PATTERN.test(version)
+        ) {
+            const error: VersionInvalid = { type: 'VersionInvalid', version };
+            this.#logger.debug('#validateVersion: VersionInvalid=%o', error);
+            return error;
+        }
+
+        return Number.parseInt(version.slice(1, -1), 10);
+    }
+
+    async #findByIdAndCheckVersion(
+        id: number,
+        version: number,
+    ): Promise<Film | MovieDoesNotExist | VersionOutdated> {
+        const filmDb = await this.#readService.findById({ id });
+        if (filmDb === undefined) {
+            const result: MovieDoesNotExist = { type: 'MovieDoesNotExist', id };
+            this.#logger.debug(
+                '#checkIdAndVersion: MovieDoesNotExist=%o',
+                result,
+            );
+            return result;
+        }
+
+        const versionDb = filmDb.version!; // eslint-disable-line @typescript-eslint/no-non-null-assertion
+        if (version < versionDb) {
+            const result: VersionOutdated = {
+                type: 'VersionOutdated',
+                id,
+                version,
+            };
+            this.#logger.debug(
+                '#checkIdAndVersion: VersionOutdated=%o',
+                result,
+            );
+            return result;
+        }
+
+        return filmDb;
     }
 }
